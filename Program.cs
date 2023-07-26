a2200add3c780b3d0b37f67f1bb4fa495c9ad84d
Kirill <rkn-sn@mail.ru>
2023-07-26 14:11:04 +0300

    Добавил домашнее задание

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void ShowNumbers(int n, int count)
+{
+    if (count < n)
+        ShowNumbers(n, count + 1);
+        Console.Write(count + " ");
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+ShowNumbers(number, count);
+
+
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers (int m, int n)
+{
+    if (m != n)
+    {
+        return SumNumbers(Math.Min(m,n), Math.Max(m,n)-1) + Math.Max(m,n);
+    }
+    else return m;
+          
+}
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = SumNumbers(m,n);
+Console.Write($"Cумма чисел равна {sum}");
+
+
+
+// Задача 68*: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Значение фукции Аккермана А({m},{n}) = {Akkerman(m,n)}");
